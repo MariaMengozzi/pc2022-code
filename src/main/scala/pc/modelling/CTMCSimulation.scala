dc320864754ff712939ee2693fa2b185b1f136df
Maria <mengozzi.maria@gmail.com>
2022-11-14 15:18:33 +0100

    chore: add some comment

--- src/main/scala/pc/modelling/CTMCSimulation.scala
+++ src/main/scala/pc/modelling/CTMCSimulation.scala
@@ -18,6 +18,8 @@ object CTMCSimulation:
           Event(t, s)
         else
           val choices = self.transitions(s) map (t => (t.rate, t.state))
+          //are the states reachable from the state in which the system is, so if I'm in SEEND the choices are:
+          // choices= HashSet((100000.0,FAIL), (100000.0,SEND), (200000.0,DONE))
           val next = Stochastics.cumulative(choices.toList)
           val sumR = next.last._1
           val choice = Stochastics.draw(next)(using rnd)
