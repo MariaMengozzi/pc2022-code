cde2d89cecd383494d1377110887eeaaf7d8d4d3
Maria <mengozzi.maria@gmail.com>
2022-11-16 11:26:11 +0100

    chore: add notes

--- src/main/scala/pc/examples/DAPGossip.scala
+++ src/main/scala/pc/examples/DAPGossip.scala
@@ -16,13 +16,13 @@ object DAPGossip:
   export pc.modelling.CTMCSimulation.*
 
   val gossipRules = DAP[Place](
-    Rule(MSet(A,A), m=>1000,  MSet(A),  MSet()),   // a|a --1000--> a
-    Rule(MSet(A),   m=>1,     MSet(A),  MSet(A)),       // a --1--> a|^a
+    Rule(MSet(A,A), m=>1000,  MSet(A),  MSet()),   // a|a --1000--> a   make sure that a node holds only one copy of a
+    Rule(MSet(A),   m=>1,     MSet(A),  MSet(A)),       // a --1--> a|^a    spread tokens "a" into neighbours
   )
   val gossipCTMC = DAP.toCTMC[ID, Place](gossipRules)
   val net = Grids.createRectangularGrid(5, 5)
   // an `a` initial on top left
-  val state = State[ID,Place](MSet(Token((0, 0), A)), MSet(), net)
+  val state = State[ID,Place](MSet(Token((0, 0), A)), MSet(), net) // state of the network at a given time, with neighbouring as a map
 
 @main def mainDAPGossip =
   import DAPGossip.*
